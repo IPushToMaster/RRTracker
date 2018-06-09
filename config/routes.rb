b8aff0055a4de70d5d55fd352a6ef5da9b3cabfb
Mike Welsh <michael.welsh@webmaxco.com>
2018-06-09 11:17:46 -0400

    gems for bootstrap, static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  # Static Routes. Default Views
+  root 'static_pages#index'
+  get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
+
+
+  
+
 end
+
